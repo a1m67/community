a8a2464ad94f3dc9b766dce4f80dd85037768a9a
a1m67 <243363113@qq.com>
2024-02-25 03:29:08 +0800

    点击邮箱链接跳转到登录页面

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -3,6 +3,7 @@ package com.nowcoder.community.service;
 import com.mysql.cj.util.LogUtils;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.MailClient;
 import org.apache.commons.lang3.StringUtils;
@@ -20,7 +21,7 @@ import java.util.Map;
 import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
     public static final String TAG = "UserService";
 
     Logger logger = LoggerFactory.getLogger(TAG);
@@ -41,78 +42,87 @@ public class UserService {
     private String contextPath;
 
 
-    public User findUserById(int id){
+    public User findUserById(int id) {
         return userMapper.selectById(id);
     }
 
-    public Map<String, Object> register(User user){
+    public Map<String, Object> register(User user) {
         Map<String, Object> map = new HashMap<>();
-        if (user == null){
+        if (user == null) {
             throw new IllegalArgumentException("参数不能为空");
         }
-        if (StringUtils.isBlank(user.getUsername())){
+        if (StringUtils.isBlank(user.getUsername())) {
 
-            map.put("usernameMsg","账号不能为空");
-            logger.debug(TAG,"账号不能为空");
+            map.put("usernameMsg", "账号不能为空");
+            logger.debug(TAG, "账号不能为空");
             return map;
         }
-        if (StringUtils.isBlank(user.getPassword())){
-            logger.debug(TAG,"密码不能为空");
-            map.put("passwordMsg","密码不能为空");
+        if (StringUtils.isBlank(user.getPassword())) {
+            logger.debug(TAG, "密码不能为空");
+            map.put("passwordMsg", "密码不能为空");
 
             return map;
         }
-        if (StringUtils.isBlank(user.getEmail())){
-            logger.debug(TAG,"邮箱不能为空");
-            map.put("emailMsg","邮箱不能为空");
+        if (StringUtils.isBlank(user.getEmail())) {
+            logger.debug(TAG, "邮箱不能为空");
+            map.put("emailMsg", "邮箱不能为空");
             return map;
         }
 
         //验证账号是否存在
-        User u =  userMapper.selectByName(user.getUsername());
-        if (null != u){
-            map.put("usernameMsg","该账号已存在!");
-            logger.debug(TAG,"该账号已存在!");
+        User u = userMapper.selectByName(user.getUsername());
+        if (null != u) {
+            map.put("usernameMsg", "该账号已存在!");
+            logger.debug(TAG, "该账号已存在!");
             return map;
         }
         //验证邮箱
-        u =  userMapper.selectByEmail(user.getEmail());
-        if (null != u){
-            map.put("emailMsg","该邮箱已存在!");
-            logger.debug(TAG,"该邮箱已存在!");
+        u = userMapper.selectByEmail(user.getEmail());
+        if (null != u) {
+            map.put("emailMsg", "该邮箱已存在!");
+            logger.debug(TAG, "该邮箱已存在!");
 
             return map;
         }
 
         //注册用户
         System.out.println("开始注册");
-        logger.debug(TAG,"开始注册");
+        logger.debug(TAG, "开始注册");
 
-        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
-        user.setPassword(CommunityUtil.md5(user.getPassword()+user.getSalt()));
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
         user.setType(0);
         user.setStatus(0);
         user.setActivationCode(CommunityUtil.generateUUID());
 
-        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png",new Random().nextInt(1000)));
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
         user.setCreateTime(new Date());
 
         userMapper.insertUser(user);
 
         // 发送激活邮件
-        logger.debug(TAG,"注册成功发送邮件");
+        logger.debug(TAG, "注册成功发送邮件");
         Context context = new Context();
-        context.setVariable("email",user.getEmail());
+        context.setVariable("email", user.getEmail());
         //http://localhost:8080/community/activation/userId/code
 
-        String url = domain + contextPath + "/activation/" + user.getId() +"/" +user.getActivationCode();
-        context.setVariable("url",url);
-        String content = templateEngine.process("/mail/activation",context);
-        mailClient.sendMail(user.getEmail(),"激活邮件",content);
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+        String content = templateEngine.process("/mail/activation", context);
+        mailClient.sendMail(user.getEmail(), "激活邮件", content);
 
         return map;
     }
 
-
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if (user.getStatus() == 1){
+            return ACTIVATION_REPEAT;
+        } else if (user.getActivationCode().equals(code)) {
+            return ACTIVATION_SUCCESS;
+        }else{
+            return ACTIVATION_FAILURE;
+        }
+    }
 
 }
