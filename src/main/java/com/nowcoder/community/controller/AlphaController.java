1ea6cea3b191e93c39f7668fea7689f82f932c8a
a1m67 <243363113@qq.com>
2024-02-28 01:01:36 +0800

    Cookie and Session Test

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -1,14 +1,17 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.service.AlphaService;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
@@ -35,16 +38,16 @@ public class AlphaController {
     }
 
     @RequestMapping("/http")
-    public void http(HttpServletRequest request, HttpServletResponse response){
+    public void http(HttpServletRequest request, HttpServletResponse response) {
         //获取请求数据
         System.out.println(request.getMethod());
         System.out.println(request.getServletPath());
         Enumeration<String> enumeration = request.getHeaderNames();
 
-        while (enumeration.hasMoreElements()){
+        while (enumeration.hasMoreElements()) {
             String name = enumeration.nextElement();
             String value = request.getHeader(name);
-            System.out.println(name+ ":" + value);
+            System.out.println(name + ":" + value);
         }
         System.out.println(request.getParameter("code"));
 
@@ -53,22 +56,22 @@ public class AlphaController {
         response.setContentType("text/html;charset=utf-8");
         //底层
         try (
-            PrintWriter writer = response.getWriter();
+                PrintWriter writer = response.getWriter();
         ) {
             writer.write("<h1>牛客网<h1>");
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     //GET请求 (一般用来获取数据)
     // students?current=1&limit=20
-    @RequestMapping(path = "/students" , method = RequestMethod.GET)
+    @RequestMapping(path = "/students", method = RequestMethod.GET)
     @ResponseBody
     public String getStudents(
             //通过RequestParam获取请求数据中对应name的值，required=false代表可有可无，defaultValue为未获取到值的默认值
-            @RequestParam(name = "current" , required = false , defaultValue = "1") int current,
-            @RequestParam(name = "limit" , required = false , defaultValue = "10") int limit){
+            @RequestParam(name = "current", required = false, defaultValue = "1") int current,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
 
         System.out.println(current);
         System.out.println(limit);
@@ -78,18 +81,18 @@ public class AlphaController {
 
     // students/123
     // 获取路径中的信息
-    @RequestMapping(path = "/students/{id}" , method = RequestMethod.GET)
+    @RequestMapping(path = "/students/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public String getStudent(@PathVariable("id") int id){
+    public String getStudent(@PathVariable("id") int id) {
         System.out.println(id);
         return "zhangSan";
     }
 
     //POST请求 (一般用来向服务器提交数据)
-    @RequestMapping( path = "/student" , method = RequestMethod.POST)
+    @RequestMapping(path = "/student", method = RequestMethod.POST)
     @ResponseBody
     //RequestMapping对应的路径与静态网页form表单的action中的路径要一致,方法中的变量名与静态网页中的 "name" 属性对应的 值一致即可获取对应的属性
-    public String saveStudent(String name , int age){
+    public String saveStudent(String name, int age) {
         System.out.println(name);
         System.out.println(age);
         return "success";
@@ -97,11 +100,11 @@ public class AlphaController {
 
     // 响应HTML数据
 
-    @RequestMapping(path = "/teacher",method = RequestMethod.GET)
-    public ModelAndView getTeacher(){
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public ModelAndView getTeacher() {
         ModelAndView mav = new ModelAndView();
-        mav.addObject("name","张三");
-        mav.addObject("age",30);
+        mav.addObject("name", "张三");
+        mav.addObject("age", 30);
         //setViewName就是设置对应的动态模板路径，文件放在template目录，写的时候不需要写，view.html不需要写后缀
         mav.setViewName("demo/view");
 
@@ -109,10 +112,10 @@ public class AlphaController {
     }
 
     //简化版本的
-    @RequestMapping(path = "/school",method = RequestMethod.GET)
-    public String getSchool(Model model){
-        model.addAttribute("name","北京大学");
-        model.addAttribute("age",100);
+    @RequestMapping(path = "/school", method = RequestMethod.GET)
+    public String getSchool(Model model) {
+        model.addAttribute("name", "北京大学");
+        model.addAttribute("age", 100);
 
 
         return "demo/view";
@@ -121,28 +124,71 @@ public class AlphaController {
     //响应JSON数据(异步请求)
     //Java对象 -> JSON字符串 -> JS对象(其他语言对象)
 
-    @RequestMapping(path = "/emp",method = RequestMethod.GET)
+    @RequestMapping(path = "/emp", method = RequestMethod.GET)
     @ResponseBody
-    public List<Map<String,Object>> getEmp(){
-        List<Map<String,Object>> list = new ArrayList<>();
-        Map<String,Object> emp = new HashMap<>();
-        emp.put("name","张三");
-        emp.put("age",33);
-        emp.put("salary",12000.00);
+    public List<Map<String, Object>> getEmp() {
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> emp = new HashMap<>();
+        emp.put("name", "张三");
+        emp.put("age", 33);
+        emp.put("salary", 12000.00);
         list.add(emp);
 
         emp = new HashMap<>();
-        emp.put("name","李四");
-        emp.put("age",23);
-        emp.put("salary",10000.00);
+        emp.put("name", "李四");
+        emp.put("age", 23);
+        emp.put("salary", 10000.00);
         list.add(emp);
 
         emp = new HashMap<>();
-        emp.put("name","王五");
-        emp.put("age",30);
-        emp.put("salary",8000.00);
+        emp.put("name", "王五");
+        emp.put("age", 30);
+        emp.put("salary", 8000.00);
         list.add(emp);
         return list;
+    }
+
+
+    //Cookie示例
+    @RequestMapping(path = "/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        //创建Cookie
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        //设置cookie生效的范围
+        cookie.setPath("/community/alpha");
+        //设置cookie生存时间
+        cookie.setMaxAge(60 * 10);
+        //发送cookie
+        response.addCookie(cookie);
+        return "set Cookie";
 
     }
+
+    @RequestMapping(path = "/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code")String code) {
+        System.out.println(code);
+        return "get Cookie";
+    }
+
+    //Session示例
+    @RequestMapping(path = "session/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session){
+        session.setAttribute("id",10086);
+        session.setAttribute("name","张三");
+        return "set Session";
+    }
+
+    @RequestMapping(path = "/session/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session){
+        System.out.println(session.getAttribute("id"));
+        System.out.println(session.getAttribute("name"));
+        return "getSession";
+    }
+
+
+
 }
