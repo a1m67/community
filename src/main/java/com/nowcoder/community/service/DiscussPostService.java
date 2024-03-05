382f8f675e442cacbbe39d67a713521e33c8f077
a1m67 <243363113@qq.com>
2024-03-06 01:27:25 +0800

    show discussPost detail

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -35,4 +35,8 @@ public class DiscussPostService {
 
         return discussPostMapper.insertDiscussPost(post);
     }
+
+    public DiscussPost findDiscussPostById(int id){
+        return discussPostMapper.selectDiscussPostById(id);
+    }
 }
