a5cb1642402db0f95050699cffafe202c914e824
a1m67 <243363113@qq.com>
2024-04-04 02:28:51 +0800

    网站数据统计

--- src/main/java/com/nowcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -1,9 +1,6 @@
 package com.nowcoder.community.config;
 
-import com.nowcoder.community.controller.interceptor.AlphaInterceptor;
-import com.nowcoder.community.controller.interceptor.LoginRequiredInterceptor;
-import com.nowcoder.community.controller.interceptor.LoginTicketInterceptor;
-import com.nowcoder.community.controller.interceptor.MessageInterceptor;
+import com.nowcoder.community.controller.interceptor.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -20,6 +17,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
     MessageInterceptor messageInterceptor;
+
+    @Autowired
+    DateInterceptor dateInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //exclude掉的可以访问
@@ -36,6 +36,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
 
         registry.addInterceptor(messageInterceptor)
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
+
+        registry.addInterceptor(dateInterceptor)
                 .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
 
     }
--- src/test/java/com/nowcoder/community/RedisTests.java
+++ src/test/java/com/nowcoder/community/RedisTests.java
@@ -5,10 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.core.BoundValueOperations;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.core.*;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -120,16 +119,16 @@ public class RedisTests {
     //编程式事务
     //在redis事务执行过程中勿查询数据，查询的数据为空，在redis执行命令前或redis执行命令后调用查询功能
     @Test
-    public void testTransactional(){
+    public void testTransactional() {
         Object obj = redisTemplate.execute(new SessionCallback() {
             @Override
             public Object execute(RedisOperations operations) throws DataAccessException {
                 String redisKey = "test:tx1";
                 //开启事务
                 operations.multi();
-                operations.opsForSet().add(redisKey,"zhangsan");
-                operations.opsForSet().add(redisKey,"lisi");
-                operations.opsForSet().add(redisKey,"wangwu");
+                operations.opsForSet().add(redisKey, "zhangsan");
+                operations.opsForSet().add(redisKey, "lisi");
+                operations.opsForSet().add(redisKey, "wangwu");
 
                 System.out.println(operations.opsForSet().members(redisKey));
 
@@ -142,5 +141,114 @@ public class RedisTests {
 
     }
 
+    // 统计20万个重复数据
+    @Test
+    public void testHyperLogLog() {
+        String redisKey = "test:hll:01";
 
+        for (int i = 0; i <= 100000 ; i++) {
+            redisTemplate.opsForHyperLogLog().add(redisKey,i);
+        }
+        for (int i = 0; i <= 100000 ; i++) {
+            int r = (int) (Math.random() * 100000 + 1);
+            redisTemplate.opsForHyperLogLog().add(redisKey,i);
+        }
+        long size = redisTemplate.opsForHyperLogLog().size(redisKey);
+        System.out.println(size);
+
+    }
+
+    // 将三组数据合并，在统计合并后重复数据的独立总数
+    @Test
+    public void testHyperLogLogUnion(){
+        String redisKey2 = "test:hll:02";
+        for (int i = 1; i <= 10000 ; i++) {
+            redisTemplate.opsForHyperLogLog().add(redisKey2,i);
+        }
+
+        String redisKey3 = "test:hll:03";
+        for (int i = 5001; i <= 15000 ; i++) {
+            redisTemplate.opsForHyperLogLog().add(redisKey3,i);
+        }
+
+        String redisKey4 = "test:hll:04";
+        for (int i = 10001; i <= 20000 ; i++) {
+            redisTemplate.opsForHyperLogLog().add(redisKey4,i);
+        }
+
+        String unionKey = "test:hll:union";
+        redisTemplate.opsForHyperLogLog().union(unionKey,redisKey4,redisKey2,redisKey3);
+
+        long size = redisTemplate.opsForHyperLogLog().size(unionKey);
+
+        System.out.println(size);
+
+
+    }
+
+
+    // 统计一组数据的布尔值
+    @Test
+    public void testBitMap() {
+        String redisKey = "test:bm:01";
+
+        //记录
+        redisTemplate.opsForValue().setBit(redisKey,1,true);
+        redisTemplate.opsForValue().setBit(redisKey,4,true);
+        redisTemplate.opsForValue().setBit(redisKey,7,true);
+
+        // 查询
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,0));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,1));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,2));
+
+        // 统计
+        Object obj = redisTemplate.execute(new RedisCallback() {
+            @Override
+            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                return redisConnection.bitCount(redisKey.getBytes());
+            }
+        });
+        System.out.println(obj);
+    }
+
+
+    // 统计3组数据的布尔值， 并对这3组数据做or运算
+    @Test
+    public void testBitMapOperation() {
+        String redisKey2 = "test:bm:02";
+
+        redisTemplate.opsForValue().setBit(redisKey2,0,true);
+        redisTemplate.opsForValue().setBit(redisKey2,1,true);
+        redisTemplate.opsForValue().setBit(redisKey2,2,true);
+        String redisKey3 = "test:bm:03";
+
+        redisTemplate.opsForValue().setBit(redisKey3,2,true);
+        redisTemplate.opsForValue().setBit(redisKey3,3,true);
+        redisTemplate.opsForValue().setBit(redisKey3,4,true);
+        String redisKey4 = "test:bm:04";
+
+        redisTemplate.opsForValue().setBit(redisKey4,4,true);
+        redisTemplate.opsForValue().setBit(redisKey4,5,true);
+        redisTemplate.opsForValue().setBit(redisKey4,6,true);
+
+        String redisKey = "test:bm:or";
+        Object obj = redisTemplate.execute(new RedisCallback() {
+            @Override
+            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                redisConnection.bitOp(RedisStringCommands.BitOperation.OR,redisKey.getBytes(),redisKey2.getBytes(),redisKey3.getBytes(),redisKey4.getBytes());
+                return redisConnection.bitCount(redisKey.getBytes());
+            }
+        });
+        System.out.println(obj);
+
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,0));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,1));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,2));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,3));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,4));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,5));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,6));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey,7));
+    }
 }
